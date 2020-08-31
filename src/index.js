436d42a044652c85f2a3b8216759995a4ff096df
jeremy-washam <Jeremy.D.Washam.19@dartmouth.edu>
2020-08-30 20:28:24 -0400

    edited imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Map } from 'immutable';
 import './style.scss';
-
-// import marked from 'marked';
 import Note from './components/note';
 import InputBar from './components/input_bar';
 import * as db from './services/datastore';
